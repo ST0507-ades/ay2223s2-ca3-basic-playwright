64c7ebafcf143f77163a8383d29c3740a44fbbab
Jeremiah Ang <jeremiahang94@gmail.com>
2022-11-04 05:58:39 +0000

    Update test to support bulk retrieve

--- tests/Insert and Retrieve.spec.js
+++ tests/Insert and Retrieve.spec.js
@@ -1,5 +1,5 @@
 const { test, expect } = require('@playwright/test');
-const { fillModules, generateModuleCodeCreditGrades } = require('../helper');
+const { generateModuleCodeCreditGrades, testRetrievedModule } = require('../helper');
 const { moduleCodes, moduleCredits, moduleGrades } = generateModuleCodeCreditGrades();
 
 test("Should add module", async function ({ page }) {
@@ -24,18 +24,10 @@ test("Should Retrieve Inserted Modules", async function ({ page }) {
     await page.locator('input[name=code]').fill(module);
     await page.locator('input[name=name]').fill(module);
     await page.locator('input[name=credit]').fill(moduleCredits[i] + '');
-    const [response] = await Promise.all([
+    await Promise.all([
       page.waitForResponse('**/modules'),
       page.locator('button[type=submit]').click()
     ]);
   }
-
-  const responses = await fillModules(page, moduleCodes);
-  for (let i = 0; i < moduleCodes.length; i++) {
-    const response = responses[i];
-    await expect(response.ok()).toBeTruthy();
-    const json = await response.json();
-    await expect(json.module.code).toEqual(moduleCodes[i]);
-    await expect(+json.module.credit).toEqual(moduleCredits[i]);
-  }
+  await testRetrievedModule(page, moduleCodes, moduleCredits);
 });
--- tests/Update by Code.spec.js
+++ tests/Update by Code.spec.js
@@ -1,5 +1,5 @@
 const { test, expect } = require('@playwright/test');
-const { fillModules, generateModuleCodeCreditGrades } = require('../helper');
+const { fillModules, generateModuleCodeCreditGrades, testRetrievedModule } = require('../helper');
 const { moduleCodes, moduleCredits, moduleGrades } = generateModuleCodeCreditGrades();
 
 function isUpdateResponse(module) {
@@ -38,25 +38,21 @@ test('Should send update to server correctly', async function ({ page }) {
 
 test("Should retrieve updated modules correctly", async function ({ page }) {
   await page.goto('/update');
+  const updatedModuleCredit = [];
   for (let i = 0; i < moduleCodes.length; i++) {
     const module = moduleCodes[i];
+    const newCredit = moduleCredits[i] + 1;
     await page.locator('input[name=code]').fill(module);
-    await page.locator('input[name=credit]').fill((moduleCredits[i] + 1) + '');
+    await page.locator('input[name=credit]').fill(newCredit + '');
     const [response] = await Promise.all([
       page.waitForResponse(isUpdateResponse(module)),
       page.locator('button[type=submit]').click()
     ]);
     await expect(response.ok()).toBeTruthy();
+    updatedModuleCredit[i] = newCredit;
   }
 
-  const responses = await fillModules(page, moduleCodes);
-  for (let i = 0; i < moduleCodes.length; i++) {
-    const response = responses[i];
-    await expect(response.ok()).toBeTruthy();
-    const json = await response.json();
-    await expect(json.module.code).toEqual(moduleCodes[i]);
-    await expect(+json.module.credit).toEqual(moduleCredits[i] + 1);
-  }
+  await testRetrievedModule(page, moduleCodes, updatedModuleCredit);
 });
 
 test('Should fail update module not in system', async function ({ page }) {
